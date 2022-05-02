074bde8b3e8b6b2d7b88652ad37ddeb8a04bcc82
Alexander Kim <avxkim@gmail.com>
2022-05-02 16:02:52 +0600

    fix: prod build marker icons

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,21 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
 
+// leaflet
+import L from 'leaflet'
+
+import icon from 'leaflet/dist/images/marker-icon.png'
+import iconShadow from 'leaflet/dist/images/marker-shadow.png'
+
+const DefaultIcon = L.icon({
+  iconUrl: icon,
+  shadowUrl: iconShadow,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+})
+
+L.Marker.prototype.options.icon = DefaultIcon
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
